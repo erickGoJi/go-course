2604eb1792d34151e09a1743048c3649eb51c0e2
erickGoJi <erickgomezlol96@gmail.com>
2024-07-15 19:18:40 -0600

    chore: Add todo functionality to structs-practice code

--- structs-practice/main.go
+++ structs-practice/main.go
@@ -8,29 +8,128 @@ import (
 	"strings"
 
 	"example.com/note/note"
+	"example.com/note/todo"
 )
 
+// saver interface is used to save the data to the file
+type saver interface {
+	Save() error
+}
+
+//	type displayer interface {
+//		Display()
+//	}
+//
+
+// outputtable interface is used to display the data, and embeds the saver interface
+type outputtable interface {
+	saver
+	Display()
+}
+
+// type outputtable interface {
+// 	Save() error
+// 	Display()
+// }
+
 func main() {
+
+	printSomething(1)
+	printSomething("Hello")
+	printSomething(1.5)
+
 	t, b, err := getNoteData()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+
+	todoContent, err := getTodoData()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	todoItem, err := todo.New(todoContent)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = outputData(todoItem)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	userNote, err := note.New(t, b)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	userNote.Display()
-	err = userNote.Save()
+
+	err = outputData(userNote)
 
 	if err != nil {
-		fmt.Println("Error saving the note: ", err)
+		fmt.Println(err)
 		return
 	}
 
-	fmt.Println("Note saved successfully!")
+}
 
+// Using an empty interface to accept any type of data
+func printSomething(value interface{}) {
+	intValue, ok := value.(int)
+	if ok {
+		fmt.Println("This is an integer: ", intValue)
+		return
+	}
+
+	floatValue, ok := value.(float64)
+	if ok {
+		fmt.Println("This is an float: ", floatValue)
+		return
+	}
+
+	stringValue, ok := value.(string)
+	if ok {
+		fmt.Println("This is an string: ", stringValue)
+		return
+	}
+	// switch value.(type) {
+	// case int:
+	// 	// ... do something
+	// 	fmt.Println("This is an integer")
+	// case float64:
+	// 	// ... do something
+	// 	fmt.Println("This is a float")
+	// case string:
+	// 	// ... do something
+	// 	fmt.Println("This is a string")
+	// default:
+	// 	// ...
+	// 	fmt.Println("Unknown type")
+	// }
+	// fmt.Println(value)
+}
+
+func outputData(data outputtable) error {
+	data.Display()
+	return saveData(data)
+}
+
+// saveData saves the data to the file
+func saveData(data saver) error {
+	err := data.Save()
+
+	if err != nil {
+		fmt.Println("Error saving the note: ", err)
+		return err
+	}
+
+	fmt.Println("Note saved successfully!")
+	return nil
 }
 
 func getNoteData() (string, string, error) {
@@ -47,6 +146,16 @@ func getNoteData() (string, string, error) {
 	}
 
 	return title, body, nil
+}
+
+func getTodoData() (string, error) {
+	content, err := getUserData("Enter the todo content: ", "Content")
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+
+	return content, nil
 }
 
 func getUserData(prompText, fieldname string) (string, error) {
---
+++ structs-practice/todo/todo.go
@@ -0,0 +1,39 @@
+package todo
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+)
+
+type Todo struct {
+	Text string `json:"text"`
+}
+
+func (todo Todo) Display() {
+	fmt.Printf("Your todo: %v\n", todo.Text)
+}
+
+func (todo Todo) Save() error {
+	filename := "todo.json"
+
+	json, err := json.Marshal(todo)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(filename, json, 0644)
+
+}
+
+// New is a constructor/creation function
+func New(content string) (Todo, error) {
+	if content == "" {
+		return Todo{}, errors.New("all fields are required")
+	}
+
+	return Todo{
+		Text: content,
+	}, nil
+}
