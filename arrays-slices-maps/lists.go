dd8f3df4507c36fa44697e4e0d5c6fa0f5775c9d
erickGoJi <erickgomezlol96@gmail.com>
2024-07-17 13:22:23 -0600

    chore: Refactor arrays-slices-maps code and add practice file

--- arrays-slices-maps/lists.go
+++ arrays-slices-maps/lists.go
@@ -2,71 +2,22 @@ package main
 
 import "fmt"
 
-type Product struct {
-	Title string
-	ID    int
-	Price float64
-}
-
-func main() {
-
-	//  1. Create a new array (!) that contains three hobbies you have
-	//     Output (print) that array in the command line.\
-	fmt.Println("Exercise 1")
-	hobbies := []string{"Programming", "Reading", "Gaming"}
-	fmt.Println(hobbies)
-	// 2) Also output more data about that array:
-	//		- The first element (standalone)
-	//		- The second and third element combined as a new list
-	fmt.Println("Exercise 2")
-	fmt.Println(hobbies[0])
-	fmt.Println(hobbies[1:])
-	// 3) Create a slice based on the first element that contains
-	//		the first and second elements.
-	//		Create that slice in two different ways (i.e. create two slices in the end)
-	fmt.Println("Exercise 3")
-	hobbiesSlice := hobbies[:2]
-	fmt.Println(hobbiesSlice)
-	// 4) Re-slice the slice from (3) and change it to contain the second
-	//		and last element of the original array.
-	fmt.Println("Exercise 4")
-	hobbiesSlice = hobbies[1:]
-	fmt.Println(hobbiesSlice)
-	// 5) Create a "dynamic array" that contains your course goals (at least 2 goals)
-	fmt.Println("Exercise 5")
-	courseGoals := []string{"Learn Go", "Build a project"}
-	fmt.Println(courseGoals)
-	// 6) Set the second goal to a different one AND then add a third goal to that existing dynamic array
-	fmt.Println("Exercise 6")
-	courseGoals[1] = "Learn React"
-	fmt.Println(courseGoals)
-	courseGoals = append(courseGoals, "Build a GRPC project")
-	fmt.Println(courseGoals)
-	// 7) Bonus: Create a "Product" struct with title, id, price and create a
-	//		dynamic list of products (at least 2 products).
-	//		Then add a third product to the existing list of products.
-	fmt.Println("Exercise 7")
-	products := []Product{ // dynamic array
-		{Title: "Book", ID: 1, Price: 10.99},
-		{Title: "Carpet", ID: 2, Price: 20.99},
-	}
-	fmt.Println(products)
-	products = append(products, Product{Title: "Table", ID: 3, Price: 30.99})
-	fmt.Println(products)
-}
-
 // Dynamic Arrays
-// func main() {
-// 	prices := []float64{10.99, 9.99}
-// 	fmt.Println(prices[0:1])
-// 	prices[1] = 9.99
+func main() {
+	prices := []float64{10.99, 9.99}
+	fmt.Println(prices[0:1])
+	prices[1] = 9.99
 
-// 	prices = append(prices, 5.99)
-// 	fmt.Println(prices)
-// 	// remove first element
-// 	prices = prices[1:]
-// 	fmt.Println(prices)
-// }
+	prices = append(prices, 5.99)
+	fmt.Println(prices)
+	// remove first element
+	prices = prices[1:]
+	fmt.Println(prices)
+
+	discountedPrices := []float64{101.99, 80.99, 20.59}
+	prices = append(prices, discountedPrices...)
+	fmt.Println(prices)
+}
 
 // Arrays
 // func main() {
