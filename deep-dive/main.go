b4f49c04cd647336f0772a3512c7eec3b61c51c7
erickGoJi <erickgomezlol96@gmail.com>
2024-07-17 19:20:36 -0600

    chore: Refactor sumup function to use variadic parameters

--- deep-dive/main.go
+++ deep-dive/main.go
@@ -2,31 +2,25 @@ package main
 
 import "fmt"
 
-// Recursion is a process in which a function calls itself as a subroutine.
-// This allows the function to be repeated several times, since it calls itself during its execution.
-// Recursion is best applied when it makes the solution clearer and easier to understand.
-// It is also useful when the problem can be broken down into smaller subproblems of the same type.
-// In Go, recursion is used in many algorithms, such as the quicksort algorithm and the binary search algorithm.
+// Using Variadic Functions
 func main() {
-	fact := factorial(5)
-	fmt.Println(fact)
+	numbers := []int{1, 10, 15}
+	// The sumup function can take a variable number of arguments.
+	sum := sumup(1, 10, 15, 40, -15)
+	// The ellipsis (...) is used to unpack the numbers slice into individual arguments.
+	anotherSum := sumup(1, numbers...)
+
+	fmt.Println(sum)
+	fmt.Println(anotherSum)
 }
 
-// Factorial is a mathematical function that multiplies a number by every number below it.
-// For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
-// The factorial of 0 is 1.
-func factorial(n int) int {
-	// Recursive case
-	if n == 0 {
-		return 1
+// Variadic functions are functions that can take a variable number of arguments.
+// The syntax for a variadic function is to use an ellipsis (...) before the type of the last parameter.
+// The variadic parameter must be the last parameter in the function signature.
+func sumup(startingValue int, numbers ...int) int {
+	sum := startingValue
+	for _, number := range numbers {
+		sum += number
 	}
-	return n * factorial(n-1)
-	// Base case
-	// result := 1 // The factorial of 0 is 1
-
-	// for i := 1; i <= n; i++ {
-	// 	result *= i
-	// }
-
-	// return result
+	return sum
 }
